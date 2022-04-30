dbe69cb2afdfe7e57fc475f46b346fa46466f961
Jose <jloai009@fiu.edu>
2022-04-30 02:19:07 -0400

    back-end: fetch country data

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,21 @@
 const express = require('express')
 const app = express()
 
+let countryData = null
+const countryNames = []
+
+const getCountryNames = () => {
+  for (data of countryData) {
+    countryNames.push(data.name.common)
+  }
+  console.log(countryNames)
+}
+
+fetch('https://restcountries.com/v3.1/all')
+  .then((res) => res.json())
+  .then((data) => {
+    countryData = data
+    getCountryNames()
+  })
+
 module.exports = app
