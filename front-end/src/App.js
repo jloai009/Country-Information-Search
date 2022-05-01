aac6a142cb51b95be75afc1065e5ffdd1bf5e4c8
Jose <jloai009@fiu.edu>
2022-05-01 00:42:48 -0400

    front-end: add element id's and class names

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -31,14 +31,18 @@ const SearchForm = ({ searchResults, setSearchResults }) => {
       searchResults.length == 0 &&
       !searchInProgress
     ) {
-      return <div> No countries were found </div>
+      return (
+        <div id="suggestions">
+          <span id="no-found">No countries were found</span>
+        </div>
+      )
     } else if (searchQuery.length !== 0 && searchResults.length > 1) {
       return (
-        <div>
+        <div id="suggestions">
           <ul>
             {searchResults.slice(0, 9).map((countryName) => (
               <li onClick={() => selectCountry(countryName)} key={countryName}>
-                {countryName}
+                <pre> {countryName}</pre>
               </li>
             ))}
           </ul>
@@ -51,14 +55,15 @@ const SearchForm = ({ searchResults, setSearchResults }) => {
 
   return (
     <div>
-      <h2>Search for a country:</h2>
+      <h2 id="search-bar-label">Search for a country:</h2>
       <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
-        <input
-          id="search-bar"
-          placeholder={'Name of country'}
-          value={searchQuery}
-          onChange={handleSearch}
-        />
+        <div id="search-bar">
+          <input
+            placeholder={'Name of country'}
+            value={searchQuery}
+            onChange={handleSearch}
+          />
+        </div>
       </form>
       <Suggestions />
     </div>
@@ -72,95 +77,100 @@ const DisplayCountry = ({ searchResults }) => {
 
   const country = searchResults[0]
   return (
-    <div>
-      <h3>
-        {country.name.official} - {country.cca2}
-      </h3>
-      <div>
-        <img src={country.flags.png} alt={'Flag of ' + country.name.common} />
-        <img
-          src={country.coatOfArms.png}
-          alt={'Coat of arms of ' + country.name.common}
-        />
+    <React.Fragment>
+      <h2>Search Results: </h2>
+      <div className="country-info">
+        <h3 className="country-title">
+          {country.name.official} - {country.cca2}
+        </h3>
+        <div>
+          <strong>Flag: </strong>
+          <img src={country.flags.png} alt={'Flag of ' + country.name.common} />
+          <strong>Coat of Arms: </strong>
+          <img
+            src={country.coatOfArms.png}
+            alt={'Coat of arms of ' + country.name.common}
+          />
+        </div>
+        <div>
+          <strong>Capital: </strong>
+          {country.capital}
+        </div>
+        <div>
+          <strong>Population: </strong>
+          {country.population.toLocaleString()}
+        </div>
+        <div>
+          <strong>Independent: </strong>
+          {country.independent ? 'Yes' : 'No'}
+        </div>
+        <div>
+          <strong>Member of the United Nations: </strong>
+          {country.unMember ? 'Yes' : 'No'}
+        </div>
+        <div>
+          <strong>Languages:</strong>
+          <ul>
+            {Object.values(country.languages).map((lang) => (
+              <li key={lang}>{lang}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="currencies">
+          <strong>Currencies: </strong>
+          <ul>
+            {Object.values(country.currencies).map((curr) => (
+              <li key={curr.name}>{curr.name}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <strong>Region: </strong>
+          {country.region}
+        </div>
+        <div>
+          <strong>Subregion: </strong>
+          {country.subregion}
+        </div>
+        <div>
+          <strong>Google Maps: </strong>
+          <a
+            href={country.maps.googleMaps}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {country.maps.googleMaps}
+          </a>
+        </div>
+        <div>
+          <strong>Continents: </strong>
+          <ul>
+            {country.continents.map((cont) => (
+              <li key={cont}>{cont}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="start-of-week">
+          <strong>Start of Week: </strong>
+          {country.startOfWeek}
+        </div>
+        <div>
+          <strong>Timezones: </strong>
+          <ul>
+            {country.timezones.map((tm) => (
+              <li key={tm}>{tm}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div>
-        <strong>Capital: </strong>
-        {country.capital}
-      </div>
-      <div>
-        <strong>Population: </strong>
-        {country.population.toLocaleString()}
-      </div>
-      <div>
-        <strong>Independent: </strong>
-        {country.independent ? 'Yes' : 'No'}
-      </div>
-      <div>
-        <strong>Member of the United Nations: </strong>
-        {country.unMember ? 'Yes' : 'No'}
-      </div>
-      <div>
-        <strong>Languages:</strong>
-        <ul>
-          {Object.values(country.languages).map((lang) => (
-            <li key={lang}>{lang}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <strong>Currencies: </strong>
-        <ul>
-          {Object.values(country.currencies).map((curr) => (
-            <li key={curr.name}>{curr.name}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <strong>Region: </strong>
-        {country.region}
-      </div>
-      <div>
-        <strong>Subregion: </strong>
-        {country.subregion}
-      </div>
-      <div>
-        <strong>Google Maps: </strong>
-        <a
-          href={country.maps.googleMaps}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {country.maps.googleMaps}
-        </a>
-      </div>
-      <div>
-        <strong>Timezones: </strong>
-        <ul>
-          {country.timezones.map((tm) => (
-            <li key={tm}>{tm}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <strong>Continents: </strong>
-        <ul>
-          {country.continents.map((cont) => (
-            <li key={cont}>{cont}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <strong>Start of week: </strong>
-        {country.startOfWeek}
-      </div>
-    </div>
+    </React.Fragment>
   )
 }
 
 const App = () => {
   const [searchResults, setSearchResults] = useState([])
   return (
-    <div>
+    <div id="App">
       <h1>Country Information Search</h1>
       <SearchForm
         searchResults={searchResults}
